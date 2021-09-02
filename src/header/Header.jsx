9ac45eb20f0879ac37f7f666b565147570cc77a2
Your Name <moseoduor86@gmail.com>
2021-09-02 16:36:41 +0300

    added styles

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import "./header.css";
-
 function Header() {
   return (
     <div className="header">
       <div className="headerTitles">
-        <span className="headerTitleSm">react & node</span>
-        <span className="headerTitleLg">blog</span>
+        <span className="headerTitleSm">React & Node</span>
+        <span className="headerTitleLg">Blog</span>
       </div>
+      <img
+        className="headerImg"
+        src="https://images.unsplash.com/photo-1547471080-7cc2caa01a7e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8a2VueWF8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
+        alt="blog"
+      />
     </div>
   );
 }
